bc17f690bd57ccc53b8a488315682c9f8d553225
Ravikumar, Abhiram (k20037424) <abhiram.ravikumar@kcl.ac.uk>
2021-05-28 12:00:03 +0100

    Refactors multiclass perceptron to handle multiple classes

--- discriminant-functions.py
+++ discriminant-functions.py
@@ -1,7 +1,7 @@
 # Discriminant Functions [week 2]
 import numpy as np
 
-def sequential_multiclass_perceptron_learning (N, augmented_matrix,eta,omega):
+def sequential_multiclass_perceptron_learning (N, augmented_matrix, eta, omega, number_of_classes ):
   # N is the number of exemplars provided in the question
   # augmented_matrix is the augmented feature vector from the question
   # eta is the learning rate given in the question
@@ -9,34 +9,52 @@ def sequential_multiclass_perceptron_learning (N, augmented_matrix,eta,omega):
 
   N_counter = 0 # counter to check for convergence
 
-  # Step 2. Initialize at wieght to 0
-  at = np.array([[0,0,0],[0,0,0],[0,0,0]])
+  #Step 2. Initialise aj for each class
+  at = np.zeros((number_of_classes, number_of_classes))
 
   for i in range(0,15):
     print ('Iteration: ',i+1)
     # Step 3. Find values of g1, g2 and g3 and then select the arg max of g
     index = i % 3
+
+    #Print updated a^t value
+    print('a^t:')
+    print(at)
     
     # Compute g value
     #print('at is ', at)
     #print('aug matrix is',augmented_matrix[:,index])
-    g1 = at[0] @ augmented_matrix[:,index]
-    g2 = at[1] @ augmented_matrix[:,index]
-    g3 = at[2] @ augmented_matrix[:,index]
-    print('g1  | g2  | g3 ')
-    print(g1, g2, g3)
+    # g1 = at[0] @ augmented_matrix[:,index]
+    # g2 = at[1] @ augmented_matrix[:,index]
+    # g3 = at[2] @ augmented_matrix[:,index]
 
-    #Select the winner
-    g_values = [g1,g2,g3]
+    g = np.empty([number_of_classes])
+    for i in range(len(g)):
+      g[i] = at[i] @ augmented_matrix[:,index]
 
+
+    print('g1 | g2 | g3')
+    print(g)
+
+    #Step 4. Select the winner
     #Logic for 0,0,0 case and similar ones where 2 gs can produce max value
-    if (g1==g2 or g1==g3 or g2==g3):
-      m = max(g_values)
-      temp = [index for index, j in enumerate(g_values) if j == m]
-      winner_class = max(temp) + 1
-    else: 
-      arg_max = max(g1,g2,g3)
-      winner_class = g_values.index(arg_max) + 1
+    seen = []
+    bRepeated = False
+    # Check if there are multiple max values, and assign the winner class accordingly
+    for number in g:
+        if number in seen:
+          bRepeated = True
+          print ("Number repeated!")
+          m = max(g)
+          temp = [index for index, j in enumerate(g) if j == m]
+          winner_class = max(temp) + 1
+        else:
+            seen.append(number)
+    #If all g values are unique, simply select the max value's class as the winner
+    if(bRepeated == False):
+      g = g.tolist()
+      arg_max = max(g)
+      winner_class = g.index(arg_max) + 1
     
     print('Winner class = ', winner_class, ', and actual class is:',omega[index])
 
@@ -59,13 +77,13 @@ def sequential_multiclass_perceptron_learning (N, augmented_matrix,eta,omega):
       N_counter +=1
       if(N_counter == N +2):
         print('Learning has converged, so stopping...')
-        print ('Final values....')
-        print('at1  | at2  | at3')
-        print(at[0],' |',at[1],' |',at[2])
+        print ('Final values of a^t after update....')
+        print('at')
+        print(at)
         break
       print ('N counter value = ', N_counter)
-    print('at1  | at2  | at3')
-    print(at[0],' |',at[1],' |',at[2])
+    print('at')
+    print(at)
     print ('=========================================================')
 
 if __name__ == "__main__":
@@ -74,6 +92,7 @@ if __name__ == "__main__":
     eta = 1
     augmented_matrix = np.array([[1,1,1,1,1],[1,2,0,-1,-1],[1,0,2,1,-1]]) # Input matrix from the question
     omega = np.array([1,1,2,2,3]) # Class labels from the question 
+    number_of_classes = 3
 
     #Call function
-    sequential_multiclass_perceptron_learning (N, augmented_matrix,eta,omega)
+    sequential_multiclass_perceptron_learning (N, augmented_matrix, eta, omega, number_of_classes)
