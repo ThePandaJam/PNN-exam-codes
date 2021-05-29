0ca2c7a8c9308ed3b21a414834bfb759e8004085
Kajetan <drakles1@gmail.com>
2021-05-29 19:06:59 +0100

    finished task6 week5

--- deep-disc-neural-nets.py
+++ deep-disc-neural-nets.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 # Deep Discriminative Neural Networks [week 5]
+from scipy import signal
 
 ERR_MSG = 'Please use proper activation function name. (relu,' \
           'lrelu,tanh,heaviside'
@@ -38,13 +39,7 @@ def tanh(x, **kwargs):
 
 
 def heaviside(x, threshold=0.1, **kwargs):
-    activation = x - threshold
-    if activation > 0:
-        return 1
-    elif activation == 0.0:
-        return 0.5
-    else:
-        return 0
+    return np.heaviside(x - threshold, 0.5)
 
 
 # Returns samples after batch normalization.
@@ -82,6 +77,46 @@ def batch_normalization(samples, beta, gamma, epsilon):
 # Returns batch normalization for a single value
 def calc_b_norm(beta, epsilon, gamma, mean, value, variance):
     return beta + gamma * ((value - mean) / np.sqrt(variance + epsilon))
+
+
+# Below code through the good offices of Hankun Wang
+def strided_len(x_len, H_len, stride):
+    return np.ceil((x_len - H_len + 1) / stride).astype(int)
+
+
+def H_dilated_len(H_len, dilation):
+    return (H_len - 1) * (dilation - 1) + H_len
+
+
+def dilate_H(H, dilation):
+    H_rows, H_cols = H[0].shape
+    H_dilated = np.zeros((H.shape[0], H_dilated_len(H_rows, dilation),
+                          H_dilated_len(H_cols, dilation)))
+    H_dilated[:, ::dilation, ::dilation] = H
+    return H_dilated
+
+
+def apply_mask(X, H, padding=0, stride=1, dilation=1):
+    # x and H can have multiple channels in the 0th dimension
+    if padding > 0:
+        X = np.pad(X, pad_width=padding, mode='constant')[1:-1]
+
+    if dilation > 1:
+        H = dilate_H(H, dilation)
+
+    H_rows, H_cols = H[0].shape
+    x_rows, x_cols = X[0].shape
+
+    fm_rows, fm_cols = strided_len(x_rows, H_rows, stride), strided_len(x_cols,
+                                                                        H_cols,
+                                                                        stride)
+    feature_map = np.empty((fm_rows, fm_cols))
+    for xf in range(fm_rows):
+        for yf in range(fm_cols):
+            xi, yi = xf * stride, yf * stride
+            receptive_region = X[:, xi: xi + H_rows, yi: yi + H_cols]
+            feature_map[xf, yf] = np.sum(H * receptive_region)
+    return feature_map
 
 
 if __name__ == '__main__':
@@ -144,3 +179,49 @@ if __name__ == '__main__':
 
     for output in batch_normalization([X1, X2, X3, X4], 0, 1, 0.1):
         print(str(output) + '\n')
+
+    # Task6 The following arrays show the feature maps that provide the input
+    # to a convolutional layer of a CNN
+    # X1 =   [ 0.2,  1. ,  0. ]
+    #        [-1. ,  0. , -0.1]
+    #        [ 0.1,  0. ,  0.1]
+    #
+    # X2 =   [ 1. ,  0.5,  0.2]
+    #        [-1. , -0.5, -0.2]
+    #        [ 0.1, -0.1,  0. ]
+    #
+    # h1 =  [ 1. , -0.1]
+    #       [ 1. , -0.1]
+    #
+    # h2 =   [ 0.5,  0.5]
+    #        [-0.5, -0.5]
+    #
+    X1 = [[0.2, 1., 0.],
+          [-1., 0., -0.1],
+          [0.1, 0., 0.1]]
+
+    X2 = [[1., 0.5, 0.2],
+          [-1., -0.5, -0.2],
+          [0.1, -0.1, 0.]]
+
+    X = np.array([X1, X2])
+
+    h1 = [[1., -0.1],
+          [1., -0.1]]
+
+    h2 = [[0.5, 0.5],
+          [-0.5, -0.5]]
+
+    H = np.array([h1, h2])
+    # Calculate the output produced by mask H when using:
+    print('padding=0 and stride=1')
+    print(apply_mask(X, H))
+
+    print('padding=1 and stride=1')
+    print(apply_mask(X, H, padding=1))
+
+    print('padding=1 and stride=2')
+    print(apply_mask(X, H, padding=1, stride=2))
+
+    print('padding=0 and stride=1 dilation=2')
+    print(apply_mask(X, H, padding=0, stride=1, dilation=2))
