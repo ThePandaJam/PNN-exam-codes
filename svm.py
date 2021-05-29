700f8dcc3e672e701d47f285631f538da4e28c14
Abhijeet Lokhande <abhijeetlokhande1996@gmail.com>
2021-05-29 15:07:56 +0100

    Pseudo inverses added

--- svm.py
+++ svm.py
@@ -21,7 +21,11 @@ def _svm(X, y, support_vectors, support_vector_class):
     rhs_arr = [1] * len(support_vector_class)
     rhs_arr.extend([0])
     rhs_arr = np.array(rhs_arr)
-    ans = rhs_arr @ np.linalg.inv(eq_arr)
+    try:
+        ans = rhs_arr @ np.linalg.inv(eq_arr)
+    except:
+        print("Unabelt to do inverse, taking pseudo inverse")
+        ans = rhs_arr @ np.linalg.pinv(eq_arr)
     print("lambda and w_0 values are ", ans)
     final_weight = []
     for idx in range(w.shape[0]):
@@ -37,14 +41,18 @@ def _svm(X, y, support_vectors, support_vector_class):
 
 # REPLACE X AND Y ACCORDING TO THE QUESTION
 # REPLACE "support_vectors" and "support_vector_class"
-X = [[3, 1], [3, -1], [7, 1], [8, 0], [1, 0], [0, 1], [-1, 0], [-2, 0]]
-y = [1, 1, 1, 1, -1, -1, -1, -1]
-# X = [[3, 3], [9, 7], [5, 5], [7, 9], [-1, -2], [-1, -4], [-3, -4], [-3, -2]]
+# X = [[3, 1], [3, -1], [7, 1], [8, 0], [1, 0], [0, 1], [-1, 0], [-2, 0]]
 # y = [1, 1, 1, 1, -1, -1, -1, -1]
+X = [[3, 3], [9, 7], [5, 5], [7, 9], [-1, -2], [-1, -4], [-3, -4], [-3, -2]]
+y = [1, 1, 1, 1, -1, -1, -1, -1]
 support_vectors = np.array(
     [[3, 1], [3, -1], [1, 0]])
 support_vector_class = np.array([1, 1, -1])
 # support_vectors = np.array([[3, 3], [-1, -2]])
 # support_vector_class = np.array([1, -1])
+# X = np.array([[1, 1], [1, -1], [-1, 1], [-1, -1]])
+# y = np.array([1, 1, -1, -1])
+# support_vectors = X
+# support_vector_class = y
 _svm(X, y, support_vectors=support_vectors,
      support_vector_class=support_vector_class)
