44a604c8fa46f13ed9ae6cc675c51e888fccf948
Kajetan <drakles1@gmail.com>
2021-05-29 19:41:17 +0100

    finish task8 week5

--- deep-disc-neural-nets.py
+++ deep-disc-neural-nets.py
@@ -119,6 +119,21 @@ def apply_mask(X, H, padding=0, stride=1, dilation=1):
     return feature_map
 
 
+def pooling(x, pooling_function, region=(2, 2), stride=1):
+    x_rows, x_cols = x[0].shape
+    pool_rows = strided_len(x_rows, region[0], stride)
+    pool_cols = strided_len(x_cols, region[1], stride)
+
+    pool = np.empty((x.shape[0], pool_rows, pool_cols))
+    for xp in range(pool_rows):
+        for yp in range(pool_cols):
+            xi = xp * stride
+            yi = yp * stride
+            pooling_region = x[:, xi: xi + region[0], yi: yi + region[1]]
+            pool[:, xp, yp] = pooling_function(pooling_region)
+    return pool
+
+
 if __name__ == '__main__':
     # Task4 The following array show the output produced by a mask in a
     # convolutional layer of a CNN
@@ -232,3 +247,20 @@ if __name__ == '__main__':
     H = np.array([h1, h2, h3])
     print('3 channel 1x1 masks applied to X1 X2 X3:')
     print(apply_mask(X, H))
+
+    # Task 8  The following array shows the input to a pooling layer of a CNN
+    X1 = [[0.2, 1., 0, 0.4],
+          [-1., 0., -0.1, -0.1],
+          [0.1, 0., -1, -0.5],
+          [0.4, -0.7, -0.5, 1]]
+
+    # Calculate the output produced by the pooling when using:
+
+    print('average pooling with a pooling region of 2x2 and stride=2')
+    print(pooling(np.array([X1]), np.mean, (2, 2), stride=2))
+
+    print('max pooling with a pooling region of 2x2 and stride=2')
+    print(pooling(np.array([X1]), np.max, (2, 2), stride=2))
+
+    print('max pooling with a pooling region of 3x3 and stride=1')
+    print(pooling(np.array([X1]), np.max, (3, 3), stride=1))
