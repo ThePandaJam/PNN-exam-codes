8ef1a6d2d78fb665ddd7770c5e1cc4875f1fd612
drdraze <ben.drazen@gmail.com>
2021-05-31 22:02:54 +0100

    M_layer_perceptrons
    Handles bias a bit diffently allowing calls to be chained to evaluate a
    deep network.
    
    Draze

--- m_layer_perceptrons_backprop.py
+++ m_layer_perceptrons_backprop.py
@@ -14,25 +14,24 @@ def eval_discriminants(weights, inputs):
 # kwargs is additional arg for activation function for example threshold
 def eval_layer(weights, inputs, ptr_activation_func, **kwargs):
     discriminants = eval_discriminants(weights, inputs)
+    print(discriminants)
     return np.transpose(ptr_activation_func(discriminants, **kwargs))
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 ## can chain these layers to evaluate a full network rapiddddddd
+## if node layers are not conencted fudge the weights as 0!
 if __name__ == '__main__':
-    features = np.array([[1, 0, 1, 0], [0, 1, 0, 1]])
+    features = np.array([[0.1, 0.9]])
 
-    input_weights = np.array([[4.8432, -0.7057, 1.9061, 2.6605, -1.1359],
-                       [0.3973, 0.4900, 1.9324, -0.4269, -5.1570],
-                       [2.1761, 0.9438, -5.4160, -0.3431, -0.2931]])
+    input_weights = np.array([[0.2, 0.5, 0],
+                       [0, 0.3, -0.7]])
 
-    hidden_weights = np.array([[2.5230, -1.1444, 0.3115, -9.9812],
-                               [2.6463, 0.0106, 11.547, 2.6479]])
-
+    hidden_weights = np.array([[-0.4, 0.8, 1.6]])
     hidden_layer = eval_layer(input_weights, features, tanh)
     print(hidden_layer)
-    output_layer = eval_layer(hidden_weights, hidden_layer, sigmoid)
+    output_layer = eval_layer(hidden_weights, hidden_layer, tanh)
     print(np.round(output_layer, 4))
 
 
